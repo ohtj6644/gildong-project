e34a776e3d7b2be914b65035d0f1b6faf8b3c16d
ohtj6644 <ohtj6644@github.com>
2023-07-02 23:19:32 +0900

    0702

--- src/main/java/com/ll/gildong/freeNotice/FreeNoticeController.java
+++ src/main/java/com/ll/gildong/freeNotice/FreeNoticeController.java
@@ -1,12 +1,32 @@
 package com.ll.gildong.freeNotice;
 
 
+import ch.qos.logback.core.model.Model;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class FreeNoticeController {
 
     private  final  FreeNoticeService freeNoticeService;
+
+
+    @GetMapping("/freeNotice/create")
+    public String createFreeNotice(){
+        return "FreeNotice_form";
+    }
+
+    @GetMapping("freeNotice/list")
+    public String freeNoticeList(Model model , @RequestParam(value = "page",defaultValue = "0")int page ,
+                                 @RequestParam(value = "kw",defaultValue = "")String kw){
+        Page<FreeNotice> paging = this.freeNoticeService.getList(page,kw);
+        int FreeNoticeCount = paging.getNumberOfElements();
+        return "FreeNotice_list";
+    }
+
+
 }
--- src/main/java/com/ll/gildong/freeNotice/FreeNoticeService.java
+++ src/main/java/com/ll/gildong/freeNotice/FreeNoticeService.java
@@ -1,12 +1,45 @@
 package com.ll.gildong.freeNotice;
 
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import javax.sql.rowset.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.core.annotation.MergedAnnotations.search;
 
 @Service
 @RequiredArgsConstructor
 public class FreeNoticeService {
 
     private  final FreeNoticeRepository freeNoticeRepository;
+
+//    private Specification<FreeNotice> search(String kw){
+//        return new Specification<>() {
+//            private static final long seriaVersionUID=1L;
+//            @Override
+//            public Predicate toPredicate(Root<FreeNotice> q, CriteriaQuery<?> query, CriteriaBuilder cd){
+//                query.distinct(true);
+//                Join<FreeNotice>
+//            }
+//        }
+//    }
+
+    public Page<FreeNotice> getList(int page, String kw) {
+        List<Sort.Order> sorts= new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page,15,Sort.by(sorts));
+//        Specification<FreeNotice> spec = search(kw);
+        return this.freeNoticeRepository.findAll(pageable);
+    }
 }
